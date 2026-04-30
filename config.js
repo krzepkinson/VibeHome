2bb58fad545591fa38fb96f9fc2823b298085f40
krzepkinson <r.juzwiak@gmail.com>
2026-05-01 00:01:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,5 @@ var CONFIG = {
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpucnJ2cWdxeGp4Z2FocGZ1cWttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MTQwMjcsImV4cCI6MjA5MjA5MDAyN30.-G3UiUGsDaca7KomB4dv9sWxDcIvFqik_xpEMAW3jAM',
     VAPID_PUBLIC_KEY: 'BL2GdHoe3QiVQrSO7NDLwbfMMcuF-07IGUFuXQgdJjd-TDl3PriVMvFnbqphZzpH48CH5IXn2hhS0sEzUAWLyoA'
 };
+// TA LINIA JEST KLUCZOWA - to ona tworzy "połączenie", którego szuka auth.js
+var supabaseClient = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);
