e580a20d2e41dc7e324300aa02e0075c9471431d
krzepkinson <r.juzwiak@gmail.com>
2026-05-08 23:55:30 +0200

    Update components.js

--- components.js
+++ components.js
@@ -167,7 +167,7 @@ window.UI = {
         </div>`;
     },
 
-    renderHealthBookTimelineItem: function(item) {
+renderHealthBookTimelineItem: function(item) {
         return `
         <div class="relative mb-5 last:mb-0">
             <div class="absolute -left-[22px] top-4 w-2.5 h-2.5 rounded-full ${item.bg.split(' ')[0]} border border-[#333537] shadow-[0_0_8px_rgb(0,0,0,0.5)]"></div>
@@ -179,8 +179,7 @@ window.UI = {
                     </div>
                     <span class="text-[9px] text-neutral-500 font-medium shrink-0 pt-0.5">${item.date.toLocaleDateString('pl-PL', { day: '2-digit', month: '2-digit', year: 'numeric' })}</span>
                 </div>
-                <p class="text-[11px] text-neutral-400 leading-relaxed ml-7">${item.desc}</p>
+                ${item.desc ? `<p class="text-[11px] text-neutral-400 leading-relaxed ml-7">${item.desc}</p>` : ''}
             </div>
         </div>`;
     }
-};
