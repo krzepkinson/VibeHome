4439169d1ad68ec172a0122383cdb74261add36d
krzepkinson <r.juzwiak@gmail.com>
2026-05-14 19:18:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const CONFIG = {
     SUPABASE_URL: "https://znrrvqgqxjxgahpfuqkm.supabase.co",
     SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpucnJ2cWdxeGp4Z2FocGZ1cWttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MTQwMjcsImV4cCI6MjA5MjA5MDAyN30.-G3UiUGsDaca7KomB4dv9sWxDcIvFqik_xpEMAW3jAM",
     VAPID_PUBLIC_KEY: "BL2GdHoe3QiVQrSO7NDLwbfMMcuF-07IGUFuXQgdJjd-TDl3PriVMvFnbqphZzpH48CH5IXn2hhS0sEzUAWLyoA",
-    APP_VERSION: "2.3.0",
-    VERSION: "2.3.0",   // ZMIANA: Zmienna potrzebna dla router.js (omijanie cache'u)
+    APP_VERSION: "2.4.0",
+    VERSION: "2.4.0",   // ZMIANA: Zmienna potrzebna dla router.js (omijanie cache'u)
     DEBUG: true,        // ZMIANA: Zmienna potrzebna dla store.js (logowanie w konsoli)
     CACHE_TTL: 30000,
     DEFAULT_AVATAR: "👤"
